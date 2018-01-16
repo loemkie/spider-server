cdafbc4a20a4ceeb09c31c5fedf3be97638ee8b3
chenqiwang <chenqiang@git.com.cn>
2018-01-16 16:28:22 +0800

    初始化

---
+++ src/com/lmk/common/model/VultrSubmitData.java
@@ -0,0 +1,15 @@
+package com.lmk.common.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * 日本服务器提交数据，汇总本地和日本服务器的爬虫数据
+ * @author loemkie
+ *
+ */
+@SuppressWarnings("serial")
+public class VultrSubmitData extends Model<VultrSubmitData> {
+	
+}
+
+
---
+++ src/com/lmk/spider/VultrSubmitDataController.java
@@ -0,0 +1,71 @@
+package com.lmk.spider;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import com.jfinal.aop.Before;
+import com.jfinal.core.Controller;
+import com.lmk.common.model.VultrSubmitData;
+
+/**
+ * 本 demo 仅表达最为粗浅的 jfinal 用法，更为有价值的实用的企业级用法
+ * 详见 JFinal 俱乐部: http://jfinal.com/club
+ * 
+ * BlogController
+ * 所有 sql 与业务逻辑写在 Model 或 Service 中，不要写在 Controller 中，养成好习惯，有利于大型项目的开发与维护
+ */
+public class VultrSubmitDataController extends Controller {
+	
+	static VultrSubmitDataService service = new VultrSubmitDataService();
+	
+	public void index() {
+		setAttr("blogPage", service.paginate(getParaToInt(0, 1), 10));
+		render("blog.html");
+	}
+	
+	public void add() {
+	}
+	
+	/**
+	 * save 与 update 的业务逻辑在实际应用中也应该放在 serivce 之中，
+	 * 并要对数据进正确性进行验证，在此仅为了偷懒
+	 */
+	public void save() {
+		VultrSubmitData vultrSubmitData = getModel(VultrSubmitData.class,"",true);
+		vultrSubmitData.set("id", UUID.randomUUID().toString().replaceAll("-", ""));
+		vultrSubmitData.set("update_date",new Date());
+		vultrSubmitData.save();
+		renderJson(vultrSubmitData);
+	}
+	
+	public void list() {
+		VultrSubmitData vultrSubmitData = new VultrSubmitData();
+		List<VultrSubmitData> vultrSubmitDataList = service.findList(vultrSubmitData);
+		String str = "";
+		for (VultrSubmitData vultrSubmitData2 : vultrSubmitDataList) {
+			str+=vultrSubmitData2.get("mobile")+"|"+vultrSubmitData2.get("card_id")+"|"+vultrSubmitData2.get("city")+"|"+vultrSubmitData2.getDate("update_date")+"\n";
+		}
+		renderText(str);
+	}
+	public void edit() {
+		setAttr("blog", service.findById(getParaToInt()));
+	}
+	
+	/**
+	 * save 与 update 的业务逻辑在实际应用中也应该放在 serivce 之中，
+	 * 并要对数据进正确性进行验证，在此仅为了偷懒
+	 */
+	@Before(BlogValidator.class)
+	public void update() {
+		getBean(VultrSubmitData.class).update();
+		redirect("/blog");
+	}
+	
+	public void delete() {
+		service.deleteById(getParaToInt());
+		redirect("/blog");
+	}
+}
+
+
